9236d97ba84e90ac374d316af29371c6a6ec10be
baalgx <bal.prxgm1407@gmail.com>
2022-10-22 12:50:31 -0500

    cepheus: Inherit common instead of common_full_phone * Ref: https://github.com/PixelOS-Pixelish/vendor_aosp/tree/thirteen/config

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
